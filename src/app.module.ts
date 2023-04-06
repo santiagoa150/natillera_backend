13f70f3fb750a493a30e64de0c9d9024c97559fb
santiagoa150 <santiago.alvarez@innventasystem.com>
2023-04-06 18:07:54 -0500

    [feature] [Santiago]: Joi to validate environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import {Module} from '@nestjs/common';
 import {ConfigModule} from '@nestjs/config';
 import {MoneyPoolModule} from './apps/money-pool/MoneyPoolModule';
+import {JoiValidationObject} from './contexts/shared/infrastructure/joi/JoiValidationObject';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({isGlobal: true}),
+        ConfigModule.forRoot({
+            isGlobal: true, validationSchema: JoiValidationObject
+        }),
         MoneyPoolModule,
     ],
     controllers: [],
---
+++ src/contexts/shared/infrastructure/joi/JoiValidationObject.ts
@@ -0,0 +1,14 @@
+import * as Joi from 'joi';
+
+export const JoiValidationObject = Joi.object({
+    APP_GLOBAL_PREFIX: Joi.string().required(),
+    APP_PORT: Joi.number().required(),
+    APP_VERSION: Joi.string().required(),
+    CONTACT_NAME: Joi.string().optional(),
+    CONTACT_URL: Joi.string().optional(),
+    CONTACT_EMAIL: Joi.string().email().optional(),
+    MONGODB_URI: Joi.string().required(),
+    SWAGGER_DESCRIPTION: Joi.string().optional(),
+    SWAGGER_PATH: Joi.string().required(),
+    SWAGGER_TITLE: Joi.string().required(),
+});
